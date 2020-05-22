b9896b099f0600766df8d11e205c8a10dc048df3
PrzemyslawMatlega <przemekmat@yahoo.com>
2020-05-22 16:06:48 +0200

    Displaing data in list mode

--- src/components/PostsList/PostsListItem/PostsListsItem.js
+++ src/components/PostsList/PostsListItem/PostsListsItem.js
@@ -3,13 +3,23 @@ import classes from './PostsListsItem.module.scss'
 
 export default function PostsListsItem(props) {
     let postClassName = classes.singlePost
+    
     if (props.postsListGridView) {
         postClassName = classes.singlePost_grid
+    }
+    function convertDateToString(date){
+        return `${date.slice(0,4)}-${date.slice(4,6)}-${date.slice(6,8)}`
     }
 
     return (
         <div className={postClassName}>
-            <img src={props.url} alt=""/>
+            <img src={props.url} alt=""/> {postClassName === classes.singlePost
+                ? <div className="data-block">
+                    <h1><b>Date:</b> {convertDateToString(props.date)}</h1>
+                    <h1><b>Category:</b> {props.formData.categoryName}</h1>
+                    <h1><b>Numbers:</b> {props.formData.numberData1} / {props.formData.numberData2} / {props.formData.numberData3}</h1>
+                </div>
+                : null}
         </div>
     )
 }
--- src/components/PostsList/PostsLists.js
+++ src/components/PostsList/PostsLists.js
@@ -21,13 +21,18 @@ export default class PostsLists extends Component {
         const dateEnd = parseFloat(this.props.appliedFilters.dateEnd.value.split("-").join(''))
         const dateStart = parseFloat(this.props.appliedFilters.dateStart.value.split("-").join(''))
         const headLineWithDates = `Posts from ${this.props.appliedFilters.dateStart.value} to ${this.props.appliedFilters.dateEnd.value}`
-        const subheadLine = `Categories: ${this.props.appliedFilters.categoryName.value.join(', ')}`
+        const subheadLine = `Categories: ${this
+            .props
+            .appliedFilters
+            .categoryName
+            .value
+            .join(', ')}`
         let contentClassName = classes.content
         if (this.props.postsListGridView) {
             contentClassName = [classes.content, classes.content__grid].join(' ')
         }
 
-        const  validatePost = singlePost => {
+        const validatePost = singlePost => {
             const validateDateStart = parseFloat(singlePost.date) >= dateStart,
                 validateDateEnd = parseFloat(singlePost.date) <= dateEnd,
                 validateCategory = this
@@ -45,11 +50,12 @@ export default class PostsLists extends Component {
             .allPosts
             .map((singlePost) => {
                 let toReturn = null;
-
                 if (validatePost(singlePost)) {
                     toReturn = <PostsListsItem
                         key={singlePost.imgName}
                         url={singlePost.url}
+                        formData={singlePost.formData}
+                        date={singlePost.date}
                         postsListGridView={this.props.postsListGridView}/>
                 }
                 return toReturn
@@ -60,8 +66,8 @@ export default class PostsLists extends Component {
         return (
             <div className={classes.postsList}>
                 <div className={classes.postsList__top}>
-                 <h1 className={classes.postsList__head}>{headLineWithDates}</h1>
-                <span className={classes.postsList__subhead}>{subheadLine}</span>
+                    <h1 className={classes.postsList__head}>{headLineWithDates}</h1>
+                    <span className={classes.postsList__subhead}>{subheadLine}</span>
                 </div>
                 <div className={contentClassName}>
                     {this.props.postsLoader === true
